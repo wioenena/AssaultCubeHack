151112cb0a91466a801c005623f323c8227da485
wioenena-q <wioenena@gmail.com>
2022-07-07 17:38:44 +0300

    .

--- AssaultCubeHack/Form1.Designer.cs
+++ AssaultCubeHack/Form1.Designer.cs
@@ -28,12 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxHealth = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelBaseAddresss = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxAmmo = new System.Windows.Forms.TextBox();
+            this.btnHealthActive = new System.Windows.Forms.Button();
+            this.btnAmmoActive = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Health:";
+            // 
+            // textBoxHealth
+            // 
+            this.textBoxHealth.Location = new System.Drawing.Point(63, 6);
+            this.textBoxHealth.Name = "textBoxHealth";
+            this.textBoxHealth.Size = new System.Drawing.Size(100, 23);
+            this.textBoxHealth.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Config:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Base Address: ";
+            // 
+            // labelBaseAddresss
+            // 
+            this.labelBaseAddresss.AutoSize = true;
+            this.labelBaseAddresss.Location = new System.Drawing.Point(107, 250);
+            this.labelBaseAddresss.Name = "labelBaseAddresss";
+            this.labelBaseAddresss.Size = new System.Drawing.Size(13, 15);
+            this.labelBaseAddresss.TabIndex = 5;
+            this.labelBaseAddresss.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Ammo:";
+            // 
+            // textBoxAmmo
+            // 
+            this.textBoxAmmo.Location = new System.Drawing.Point(63, 30);
+            this.textBoxAmmo.Name = "textBoxAmmo";
+            this.textBoxAmmo.Size = new System.Drawing.Size(100, 23);
+            this.textBoxAmmo.TabIndex = 1;
+            // 
+            // btnHealthActive
+            // 
+            this.btnHealthActive.Location = new System.Drawing.Point(169, 6);
+            this.btnHealthActive.Name = "btnHealthActive";
+            this.btnHealthActive.Size = new System.Drawing.Size(75, 23);
+            this.btnHealthActive.TabIndex = 6;
+            this.btnHealthActive.Text = "Active";
+            this.btnHealthActive.UseVisualStyleBackColor = true;
+            this.btnHealthActive.Click += new System.EventHandler(this.healthActive_Click);
+            // 
+            // btnAmmoActive
+            // 
+            this.btnAmmoActive.Location = new System.Drawing.Point(169, 30);
+            this.btnAmmoActive.Name = "btnAmmoActive";
+            this.btnAmmoActive.Size = new System.Drawing.Size(75, 23);
+            this.btnAmmoActive.TabIndex = 6;
+            this.btnAmmoActive.Text = "Active";
+            this.btnAmmoActive.UseVisualStyleBackColor = true;
+            this.btnAmmoActive.Click += new System.EventHandler(this.btnAmmoActive_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAmmoActive);
+            this.Controls.Add(this.btnHealthActive);
+            this.Controls.Add(this.labelBaseAddresss);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxAmmo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxHealth);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBoxHealth;
+        private Label label2;
+        private Label label3;
+        private Label labelBaseAddresss;
+        private Label label4;
+        private TextBox textBoxAmmo;
+        private Button btnHealthActive;
+        private Button btnAmmoActive;
     }
 }
--- AssaultCubeHack/Form1.cs
+++ AssaultCubeHack/Form1.cs
@@ -1,3 +1,4 @@
+
 namespace AssaultCubeHack
 {
     public partial class Form1 : Form
@@ -5,6 +6,51 @@ namespace AssaultCubeHack
         public Form1()
         {
             InitializeComponent();
+        }
+
+        MemoryX.Memory memoryX = new MemoryX.Memory();
+
+        private long baseAddrss;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(!this.memoryX.GetProcessHandle("ac_client")) {
+                MessageBox.Show("Game is not running!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+
+            this.baseAddrss = this.memoryX.GetBaseAddress("ac_client.exe");
+            this.labelBaseAddresss.Text = "0x" + this.baseAddrss.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int newHealth = int.Parse(this.textBoxHealth.Text);
+                int ammo =
+                this.memoryX.WriteMemoryPointer(this.baseAddrss + 0x109B74, new int[] { 0xF8 }, newHealth);
+                MessageBox.Show(this.memoryX.ReadMemoryPointerInt(this.baseAddrss + 0x0010A280, new int[] { 0x8, 0xDCC, 0x34, 0x6C, 0xA4, 0x438 }).ToString());
+            } catch { }
+        }
+
+        private void healthActive_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int newHealth = int.Parse(this.textBoxHealth.Text);
+                this.memoryX.WriteMemoryPointer(this.baseAddrss + 0x109B74, new int[] { 0xF8 }, newHealth);
+            } catch { }
+        }
+
+        private void btnAmmoActive_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int newAmmo = int.Parse(this.textBoxAmmo.Text);
+                this.memoryX.WriteMemoryPointer(this.baseAddrss + 0x0010A280, new int[] { 0x8, 0xDCC, 0x34, 0x6C, 0xA4, 0x438 }, newAmmo);
+            }
+            catch { }
         }
     }
 }
